d3c3f4d94e248ab27f3f5b3ea35c8ae138bccc25
张威年 <zhangweinian.cqupt@gmail.com>
2026-08-19 12:29:58 +0800

    更新

---
+++ .devcontainer/.format/.cmake-format.yaml.py
@@ -0,0 +1,27 @@
+# .cmake-format.py
+
+format = {
+    "line_width": 120,
+    "dangle_parens": True,
+    "use_tabchars": True,
+    "tab_size": 4,
+    "separate_fn_name_with_space": True,
+    "max_subgroups_hwrap": 2,
+    "dangle_align": "prefix",
+    "keyword_case": "upper",
+    "command_case": "lower",
+    "max_pargs_hwrap": 3,
+}
+
+markup = {
+    "canonicalize_hashrulers": True,
+    "enable_markup": False,
+}
+
+encode = {
+    "input_encoding": "utf-8",
+    "output_encoding": "utf-8",
+    "newline": "lf",
+    "ensure_newline_at_eof": True,
+    "preserve_trailing_whitespace": False,
+}
